b9cf34bc7d6e7cb6b8d5ea5c31d82b7f2221af6b
Masiluy <71081063+kittyofheaven@users.noreply.github.com>
2021-09-11 20:53:30 +0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -39,31 +39,55 @@ def hangman():
         print('exiting')
         time.sleep(1)
         exit()
-        
-    #definisiin tries
+
+    #definisiin macam macam var
     tries = 0 
+    guessed_letter = []
+    wrong_guessed_letter = []
 
     while tries != 8 :
         #print hangmannya
         print(hangman_list[tries])
 
+        print('guessed letter : ' + str(guessed_letter))
+        print('wrong letter : ' + str(wrong_guessed_letter))
+
         #kasih tau kata nya
         print('your word is : ' + word)
         player_guess = input('your letter guess : ').upper()
-        print(player_guess)
+        #print(player_guess)
 
-        if bool(re.compile(player_guess).search(word.upper())) : 
+        if bool(re.compile(player_guess).search(word.upper())) and len(player_guess) == 1 : 
             print('you guessed 1 letter')
+            guessed_letter.append(player_guess)
+
+        elif len(player_guess) > 1 :
+            print('invalid guess, try again!')
         
         else :
             print('you guessed wrong letter')
             tries = tries + 1
-
+            wrong_guessed_letter.append(player_guess)
+            
+        time.sleep(0.5)
+        
     if tries == 8 :
+        clear()
         print ('you lose')
         print ('the word was : ' + word )
     else :
+        clear()
         print('you win, you guessed the word')
+    
+    #play again?
+    play_again = input('Play again? Y / N : ')
+    if play_again == 'Y' or play_again == 'y': 
+        clear()
+        hangman()
+    else :
+        print('Thank you for playing!!!') 
+        time.sleep(2)
+        clear()
 
 
 hangman()
