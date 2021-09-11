fa65cf716ce5aabb053cd32f1efd9dbc950f54be
Masiluy <71081063+kittyofheaven@users.noreply.github.com>
2021-09-11 20:07:46 +0700

    update the hangman

--- hangman.py
+++ hangman.py
@@ -21,13 +21,40 @@ def hangman():
     print("1. Makanan")
     print("2. Hewan")
     
+    #pilihan tema
     tema = {'1' : makanan, 'makanan' : makanan, 'Makanan' : makanan, 
             '2' : hewan,'hewan' : hewan, 'Hewan' :hewan
             }
     
+    #ambil kata
     theme_choice = raw_input('Pilih tema : ')
     word = get_word(tema[theme_choice])
     clear()
-    print(word)
+    
+    #definisiin tries
+    tries = 0 
+
+    while tries != 8 :
+        #print hangmannya
+        print(hangman_list[tries])
+
+        #kasih tau kata nya
+        print('your word is : ' + word)
+        player_guess = raw_input('your letter guess : ').upper()
+        print(player_guess)
+
+        if bool(re.compile(player_guess).search(word.upper())) : 
+            print('you guessed 1 letter')
+        
+        else :
+            print('you guessed wrong letter')
+            tries = tries + 1
+
+    if tries == 8 :
+        print ('you lose')
+        print ('the word was : ' + word )
+    else :
+        print('you win, you guessed the word')
+
 
 hangman()
