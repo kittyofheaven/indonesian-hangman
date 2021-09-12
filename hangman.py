2b203f2488e5b35c1081335c09aa7479b63bab20
Masiluy <71081063+kittyofheaven@users.noreply.github.com>
2021-09-12 17:06:33 +0700

    FINISHED HANGMAN.PY
    HAHAHA HAHAHAHAHHA HAHAHAHHAHA I WIN, I WINNNNN

--- hangman.py
+++ hangman.py
@@ -51,8 +51,9 @@ def hangman():
     question = {}
     word_completion = '_' * len(word)
     list_word_completion = split(word_completion)
+    list_word = split(word.upper())
 
-    while tries != 8 :
+    while tries != 8 and list_word != list_word_completion :
         #print hangmannya
         print(hangman_list[tries])
 
@@ -60,39 +61,52 @@ def hangman():
         print('wrong letter : ' + str(wrong_guessed_letter))
 
         #kasih tau kata nya
-        print('your word is : ' + word_completion)
-        player_guess = input('your letter guess : ').upper()
-        #print(player_guess)
-
+        wordo_completion = ''.join(list_word_completion)
+        print('your word is : ' + wordo_completion)
         
+        if bool(re.compile(word[0].upper()).search(wordo_completion)) == False :
 
+            player_guess = word[0].upper()
+            for idx, item in enumerate(list_word):
+                if player_guess in item:
+                    list_word_completion[idx] = player_guess
+            guessed_letter.append(player_guess)
 
-        if bool(re.compile(player_guess).search(word.upper())) and len(player_guess) == 1 : 
-            
-            if player_guess in guessed_letter :
-                print("you already guessed " + player_guess)
-            else :
-                print('you guessed 1 letter')
-                guessed_letter.append(player_guess)
-        
-
-        elif len(player_guess) > 1 :
-            print('invalid guess, try again!')
-        
         else :
-            print('you guessed wrong letter')
-            tries = tries + 1
-            wrong_guessed_letter.append(player_guess)
+
+            player_guess = input('your letter guess : ').upper()
+
+            if bool(re.compile(player_guess).search(word.upper())) and len(player_guess) == 1 : 
+                
+                if player_guess in guessed_letter :
+                    print("you already guessed " + player_guess)
+                else :
+                    for idx, item in enumerate(list_word):
+                        if player_guess in item:
+                            list_word_completion[idx] = player_guess
+
+                    print('you guessed 1 letter')
+                    guessed_letter.append(player_guess)
+
+            elif len(player_guess) > 1 :
+                print('invalid guess, try again!')
             
-        time.sleep(0.5)
+            else :
+                if player_guess in wrong_guessed_letter :
+                    print("you already guessed " + player_guess)
+                else :
+                    print('you guessed wrong letter')
+                    tries = tries + 1
+                    wrong_guessed_letter.append(player_guess)
+                
+            time.sleep(0.5)
         
-    if tries == 8 :
+    if tries == 8 and list_word != list_word_completion:
         clear()
-        print ('you lose')
-        print ('the word was : ' + word )
+        print ('you lose, you guessed the word, the word was ' + word)
     else :
         clear()
-        print('you win, you guessed the word')
+        print('you win, you guessed the word, the word was ' + word)
     
     #play again?
     play_again = input('Play again? Y / N : ')
@@ -103,9 +117,5 @@ def hangman():
         print('Thank you for playing!!!') 
         time.sleep(2)
         clear()
-
-#mekanisme gamenya ydah tapi belum ditentukan kondisi menangnya gimana
-#belum juga nunjukin _ _ _ _ sesuai length wordnya 
-
 
 hangman()
