da0311846c8500f70d0d093643ccde61ff3b52e5
shironeko <71081063+kittyofheaven@users.noreply.github.com>
2021-09-12 17:16:29 +0700

    Update wordlist.py

--- wordlist.py
+++ wordlist.py
@@ -1,2 +1,2 @@
 makanan = ['mie ayam', 'soto', 'soto kudus', 'soto lamongan', 'bihun', 'kari ayam', 'sop ikan', 'gulai',]
-hewan = ['sapi', 'babi', 'ayam', 'kambing', 'kalkun', 'bebek', 'ikan', 'elang', 'kakaktua']
+hewan = ['sapi', 'babi', 'ayam', 'kambing', 'kalkun', 'bebek', 'ikan', 'elang', 'kakaktua', 'anjing']
